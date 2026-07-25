5178eb8029ac0247b01cb0f00655faa4177151d9
devashmit <devvv0264@gmail.com>
2026-07-25 20:30:11 +0545

    db(schema): add headquarters, area, and population fields to District model

--- server/models/District.js
+++ server/models/District.js
@@ -20,6 +20,24 @@ const District = sequelize.define('District', {
     field: 'cdo_name',
     allowNull: true
   },
+  assistantCdo: {
+    type: DataTypes.STRING,
+    field: 'assistant_cdo',
+    allowNull: true
+  },
+  headquarters: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  areaSqKm: {
+    type: DataTypes.STRING,
+    field: 'area_sq_km',
+    allowNull: true
+  },
+  population: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
   daoAddress: {
     type: DataTypes.TEXT,
     field: 'dao_address',
@@ -30,9 +48,44 @@ const District = sequelize.define('District', {
     field: 'dao_contact',
     allowNull: true
   },
+  daoEmail: {
+    type: DataTypes.STRING,
+    field: 'dao_email',
+    allowNull: true
+  },
+  daoWebsite: {
+    type: DataTypes.STRING,
+    field: 'dao_website',
+    allowNull: true
+  },
   daoOfficeHours: {
     type: DataTypes.STRING,
     field: 'dao_office_hours',
+    allowNull: true
+  },
+  municipalitiesCount: {
+    type: DataTypes.INTEGER,
+    field: 'municipalities_count',
+    allowNull: true
+  },
+  ruralMunicipalitiesCount: {
+    type: DataTypes.INTEGER,
+    field: 'rural_municipalities_count',
+    allowNull: true
+  },
+  policeContact: {
+    type: DataTypes.STRING,
+    field: 'police_contact',
+    allowNull: true
+  },
+  emergencyContact: {
+    type: DataTypes.STRING,
+    field: 'emergency_contact',
+    allowNull: true
+  },
+  mayorName: {
+    type: DataTypes.STRING,
+    field: 'mayor_name',
     allowNull: true
   }
 }, {

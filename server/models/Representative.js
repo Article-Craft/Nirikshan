b6339f717824afa00b90f3426ac6b82e8ae114a5
devashmit <devvv0264@gmail.com>
2026-07-30 22:30:35 +0545

    Merge pull request #19 from devashmit/Ashmit

--- server/models/Representative.js
+++ server/models/Representative.js
@@ -1,3 +1,7 @@
+/**
+ * Representative Model representing elected politicians in Nepal.
+ * Stores attendance, asset declarations, tenure, and verified pledge metrics.
+ */
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
@@ -47,6 +51,53 @@ const Representative = sequelize.define('Representative', {
   bio: {
     type: DataTypes.TEXT,
     allowNull: true
+  },
+  education: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  electionHistory: {
+    type: DataTypes.TEXT,
+    field: 'election_history',
+    allowNull: true
+  },
+  assets: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  declarations: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  votingRecord: {
+    type: DataTypes.TEXT,
+    field: 'voting_record',
+    allowNull: true
+  },
+  promisesCompleted: {
+    type: DataTypes.INTEGER,
+    field: 'promises_completed',
+    defaultValue: 0
+  },
+  promisesInProgress: {
+    type: DataTypes.INTEGER,
+    field: 'promises_in_progress',
+    defaultValue: 0
+  },
+  promisesBroken: {
+    type: DataTypes.INTEGER,
+    field: 'promises_broken',
+    defaultValue: 0
+  },
+  promisesDelayed: {
+    type: DataTypes.INTEGER,
+    field: 'promises_delayed',
+    defaultValue: 0
+  },
+  ratingValue: {
+    type: DataTypes.DECIMAL(3, 2),
+    field: 'rating_value',
+    defaultValue: 4.0
   }
 }, {
   tableName: 'representatives'
